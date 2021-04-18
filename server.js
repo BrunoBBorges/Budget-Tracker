6943d17e2ff56b38115369620f24c589e4bb41b9
Bruno Borges <brunobborges68@gmail.com>
2021-04-18 16:19:46 -0400

    adding mongodb atlas connection

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
-
+const compression = require("compression"); 
 const PORT = 3000;
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://BrunoBorges:Tz7v5rgmh2EdI67j@cluster0.zg8iv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 const app = express();
 
